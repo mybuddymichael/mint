32957bf1cd6591c0c4102b4ae4162ed3bc0b6251
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2025-11-25 14:26:46 -0800

    refactor: Use strings.Builder to compose text

--- format_issue.go
+++ format_issue.go
@@ -9,54 +9,38 @@ import (
 // PrintIssueDetails prints full issue details including ID, Title, Status,
 // Dependencies, Blocks, and Comments
 func PrintIssueDetails(w io.Writer, issue *Issue, store *Store) error {
-	if _, err := fmt.Fprintf(w, "\033[1m\033[38;5;5mID\033[0m      %s\n", store.FormatID(issue.ID)); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprintf(w, "\033[1m\033[38;5;5mTitle\033[0m   %s\n", issue.Title); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprintf(w, "\033[1m\033[38;5;5mStatus\033[0m  %s\n", issue.Status); err != nil {
-		return err
-	}
+	var b strings.Builder
+	fmt.Fprintf(&b, "\033[1m\033[38;5;5mID\033[0m      %s\n", store.FormatID(issue.ID))
+	fmt.Fprintf(&b, "\033[1m\033[38;5;5mTitle\033[0m   %s\n", issue.Title)
+	fmt.Fprintf(&b, "\033[1m\033[38;5;5mStatus\033[0m  %s\n", issue.Status)
 	if len(issue.DependsOn) > 0 {
-		if _, err := fmt.Fprintln(w, "\033[1m\033[38;5;5mDepends on\033[0m"); err != nil {
-			return err
-		}
+		fmt.Fprintln(&b, "\033[1m\033[38;5;5mDepends on\033[0m")
 		for _, depID := range issue.DependsOn {
 			dep, err := store.GetIssue(depID)
 			if err != nil {
 				return err
 			}
-			if _, err := fmt.Fprintf(w, "  %s %s\n", store.FormatID(dep.ID), dep.Title); err != nil {
-				return err
-			}
+			fmt.Fprintf(&b, "  %s %s\n", store.FormatID(dep.ID), dep.Title)
 		}
 	}
 	if len(issue.Blocks) > 0 {
-		if _, err := fmt.Fprintln(w, "\033[1m\033[38;5;5mBlocks\033[0m"); err != nil {
-			return err
-		}
+		fmt.Fprintln(&b, "\033[1m\033[38;5;5mBlocks\033[0m")
 		for _, blockID := range issue.Blocks {
 			blocked, err := store.GetIssue(blockID)
 			if err != nil {
 				return err
 			}
-			if _, err := fmt.Fprintf(w, "  %s %s\n", store.FormatID(blocked.ID), blocked.Title); err != nil {
-				return err
-			}
+			fmt.Fprintf(&b, "  %s %s\n", store.FormatID(blocked.ID), blocked.Title)
 		}
 	}
 	if len(issue.Comments) > 0 {
-		if _, err := fmt.Fprintln(w, "\033[1m\033[38;5;5mComments\033[0m"); err != nil {
-			return err
-		}
+		fmt.Fprintln(&b, "\033[1m\033[38;5;5mComments\033[0m")
 		for _, comment := range issue.Comments {
-			if _, err := fmt.Fprintf(w, "  %s\n", comment); err != nil {
-				return err
-			}
+			fmt.Fprintf(&b, "  %s\n", comment)
 		}
 	}
-	return nil
+	_, err := fmt.Fprint(w, b.String())
+	return err
 }
 
 // printIssueList prints a list of issues with aligned formatting
